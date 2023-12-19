ef69755b0e19e2dae156565a7202f8813c974d7d
2017libin <1963482624@qq.com>
2023-12-20 01:25:41 +0800

    update multithreads test

--- test/test_ecs_sm2_multithreads.c
+++ test/test_ecs_sm2_multithreads.c
@@ -51,22 +51,8 @@ static int ecs_sm2_gen_test(int thread_num) {
 }
 
 static int ecs_sm2_sign_test(int thread_num) {
-    // 为参数分配空间
-    if (core_init() != RLC_OK) {
-        printf("参数init失败！\n");
-        core_clean();
-        return 1;
-    }
-
-    // 为参数设置具体的值
-    if(ec_param_set_any() != RLC_OK){
-        printf("参数set失败！\n");
-        core_clean();
-        return 1;
-    }
-
     int code = RLC_ERR;
-    bn_t d, r, s;
+    bn_t d, r[1000], s[1000], e;
     ec_t q;
     uint8_t m[5] = { 0, 1, 2, 3, 4 }, h[RLC_MD_LEN];
 
@@ -76,31 +62,37 @@ static int ecs_sm2_sign_test(int thread_num) {
     ec_null(q);
 
     bn_new(d);
-    bn_new(r);
-    bn_new(s);
+//
+//    bn_new(r);
+//    bn_new(s);
+    bn_new(e);
     ec_new(q);
+
 
     // 生成公私钥
     cp_sm2_gen(d, q);
-
+    bn_rand_mod(e, d);
     // 签名
 
 #if 1
     // 多线程测试
     double begin,end;
-    size_t count = 10000;
+    size_t count = 1000;
 
+    for (int i = 0; i < count; ++i) {
+        bn_new(r[i]);
+        bn_new(s[i]);
+    }
     // 签名性能测试
     begin = omp_get_wtime();
 #pragma omp parallel for num_threads(thread_num)
     for(int i = 0; i < count;i++){
-        cp_sm2_sig(r, s, m, sizeof(m), 0, d);
+        cp_sm2_sig_with_hash(r[i], s[i], e, d);
     }
     end = omp_get_wtime();
     printf("sign - %d threads: run %d times, , total time: %f s, per second run %f tims\n", \
 			thread_num, count, (end-begin), count/(end-begin));
 #endif
-
 
     end:
     bn_free(d);
@@ -156,6 +148,8 @@ static int ecs_sm2_very_test(int thread_num) {
     ec_free(q);
     return 0;
 }
+
+
 int main() {
     // 为参数分配空间
     if (core_init() != RLC_OK) {
@@ -170,13 +164,13 @@ int main() {
         return 1;
     }
 
-//    ecs_sm2_gen_test(1);
+    //    ecs_sm2_gen_test(1);
 //    ecs_sm2_gen_test(2);
 //    ecs_sm2_gen_test(4);
 //    ecs_sm2_gen_test(8);
 //    ecs_sm2_gen_test(12);
 //    ecs_sm2_gen_test(16);
-    ecs_sm2_sign_test(1);
+    ecs_sm2_sign_test(4);
     ecs_sm2_sign_test(2);
     ecs_sm2_sign_test(4);
     ecs_sm2_sign_test(8);
